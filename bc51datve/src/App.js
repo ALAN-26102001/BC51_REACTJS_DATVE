b13eeef2b31a94fbd23ed1c40dbbbd56c99ea347
ALAN <ntthanhnga2610@gmail.com>
2023-08-25 01:50:38 +0700

    upload source

---
+++ bc51datve/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BookingTickets from "./Booking_Redux/BookingTickets";
+
+function App() {
+	return (
+		<div className="App">
+			<BookingTickets />
+		</div>
+	);
+}
+
+export default App;
